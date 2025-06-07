7cd2a5bd930eb98fd3a6a726f25f6d066172386f
kirill4586 <kirill4586@proton.me>
2025-06-07 05:56:00 +0300

    ячсчяс

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-ort Bot, Dispatcher, F, types
+import os
+import logging
+import asyncio
+import aiohttp
+import shutil
+import sys
+import time
+import json
+import websockets
+import socketio
+from aiohttp import web
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import (
