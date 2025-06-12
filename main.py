0d52d09bcf9dc138dfaa6598b89a923e01659f6f
kirill4586 <kirill4586@proton.me>
2025-06-12 21:12:52 +0300

    75654

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ client_options = ClientOptions(
 bot = Bot(
     token=BOT_TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+	)
 dp = Dispatcher(storage=MemoryStorage())
 dp.update.middleware(CallbackTimeoutMiddleware())
 os.makedirs(UPLOAD_DIR, exist_ok=True)
